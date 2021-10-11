fb58fb020b2b50873a20597644e55caef6b6a8ec
stvalenta <stvalenta@seznam.cz>
2021-10-11 11:31:08 +0200

    Rozdělení

---
+++ narozky.js
@@ -0,0 +1,130 @@
+function myFunctionx() {
+  if (((JanDlouhy.month - JanDlouhy.thismonth) == 1) || ((JanDlouhy.month - JanDlouhy.thismonth) == 2)){
+    document.getElementById("jmenoj").innerHTML = JanDlouhy.first + " " + JanDlouhy.last;
+    document.getElementById("vekj").innerHTML = JanDlouhy.age;
+    document.getElementById("narozeninyj").innerHTML = JanDlouhy.date.getDate() + "." + JanDlouhy.date.getMonth() + 
+    "." + JanDlouhy.date.getFullYear();
+    document.getElementById("svatekj").innerHTML = JanDlouhy.sdate.getDate() + "." + JanDlouhy.sdate.getMonth();
+    document.getElementById("narozeninyj").style.backgroundColor = "red";
+  }
+  else{
+  	document.getElementById("JanDlouhy").style.display = "none";
+  }
+}
+function myFunction1() {
+  if (((PetrSteier.month - PetrSteier.thismonth) == 1) || ((PetrSteier.month - PetrSteier.thismonth) == 2)){
+    document.getElementById("jmeno").innerHTML = PetrSteier.first + " " + PetrSteier.last;
+    document.getElementById("vek").innerHTML = PetrSteier.age;
+    document.getElementById("narozeniny").innerHTML = PetrSteier.date.getDate() + "." + PetrSteier.date.getMonth() + 
+    "." + PetrSteier.date.getFullYear();
+    document.getElementById("svatek").innerHTML = PetrSteier.sdate.getDate() + "." + PetrSteier.sdate.getMonth();
+    document.getElementById("narozeniny").style.backgroundColor = "red";
+  }
+  else{
+    document.getElementById("PetrSteier").style.display = "none";
+  }
+}
+function myFunction2() {
+  if (((PavlaSteierova.month - PavlaSteierova.thismonth) == 1) || ((PavlaSteierova.month - PavlaSteierova.thismonth) == 2)){
+    document.getElementById("1jmeno").innerHTML = PavlaSteierova.first + " " + PavlaSteierova.last;
+    document.getElementById("1vek").innerHTML = PavlaSteierova.age;
+    document.getElementById("1narozeniny").innerHTML = PavlaSteierova.date.getDate() + "." + PavlaSteierova.date.getMonth() + 
+    "." + PavlaSteierova.date.getFullYear();
+    document.getElementById("1svatek").innerHTML = PavlaSteierova.sdate.getDate() + "." + PavlaSteierova.sdate.getMonth();
+    document.getElementById("1narozeniny").style.backgroundColor = "red";
+  }
+  else{
+    document.getElementById("PavlaSteierova").style.display = "none";
+  }
+}
+function myFunction3() {
+  if (((EvaKorbuthova.month - EvaKorbuthova.thismonth) == 1) || ((EvaKorbuthova.month - EvaKorbuthova.thismonth) == 2)){
+    document.getElementById("2jmeno").innerHTML = EvaKorbuthova.first + " " + EvaKorbuthova.last;
+    document.getElementById("2vek").innerHTML = EvaKorbuthova.age;
+    document.getElementById("2narozeniny").innerHTML = EvaKorbuthova.date.getDate() + "." + EvaKorbuthova.date.getMonth() + 
+    "." + EvaKorbuthova.date.getFullYear();
+    document.getElementById("2svatek").innerHTML = EvaKorbuthova.sdate.getDate() + "." + EvaKorbuthova.sdate.getMonth();
+    document.getElementById("2narozeniny").style.backgroundColor = "red";
+  }
+  else{
+    document.getElementById("EvaKorbuthova").style.display = "none";
+  }
+}
+function myFunction4() {
+  if (((AnetaSteierova.month - AnetaSteierova.thismonth) == 1) || ((AnetaSteierova.month - AnetaSteierova.thismonth) == 2)){
+    document.getElementById("3jmeno").innerHTML = AnetaSteierova.first + " " + AnetaSteierova.last;
+    document.getElementById("3vek").innerHTML = AnetaSteierova.age;
+    document.getElementById("3narozeniny").innerHTML = AnetaSteierova.date.getDate() + "." + AnetaSteierova.date.getMonth() + 
+    "." + AnetaSteierova.date.getFullYear();
+    document.getElementById("3svatek").innerHTML = AnetaSteierova.sdate.getDate() + "." + AnetaSteierova.sdate.getMonth();
+    document.getElementById("3narozeniny").style.backgroundColor = "red";
+  }
+  else{
+    document.getElementById("AnetaSteierova").style.display = "none";
+  }
+}
+function myFunction5() {
+  if (((EliskaSteierova.month - EliskaSteierova.thismonth) == 1) || ((EliskaSteierova.month - EliskaSteierova.thismonth) == 2)){
+    document.getElementById("4jmeno").innerHTML = EliskaSteierova.first + " " + EliskaSteierova.last;
+    document.getElementById("4vek").innerHTML = EliskaSteierova.age;
+    document.getElementById("4narozeniny").innerHTML = EliskaSteierova.date.getDate() + "." + EliskaSteierova.date.getMonth() + 
+    "." + EliskaSteierova.date.getFullYear();
+    document.getElementById("4svatek").innerHTML = EliskaSteierova.sdate.getDate() + "." + EliskaSteierova.sdate.getMonth();
+    document.getElementById("4narozeniny").style.backgroundColor = "red";
+  }
+  else{
+    document.getElementById("EliskaSteierova").style.display = "none";
+  }
+}
+function myFunction6() {
+  if (((OlgaValentova.month - OlgaValentova.thismonth) == 1) || ((OlgaValentova.month - OlgaValentova.thismonth) == 2)){
+    document.getElementById("5jmeno").innerHTML = OlgaValentova.first + " " + OlgaValentova.last;
+    document.getElementById("5vek").innerHTML = OlgaValentova.age;
+    document.getElementById("5narozeniny").innerHTML = OlgaValentova.date.getDate() + "." + OlgaValentova.date.getMonth() + 
+    "." + OlgaValentova.date.getFullYear();
+    document.getElementById("5svatek").innerHTML = OlgaValentova.sdate.getDate() + "." + OlgaValentova.sdate.getMonth();
+    document.getElementById("5narozeniny").style.backgroundColor = "red";
+  }
+  else{
+    document.getElementById("OlgaValentova").style.display = "none";
+  }
+}
+function myFunction7() {
+  if (((StanislavValentaIng.month - StanislavValentaIng.thismonth) == 1) || ((StanislavValentaIng.month - StanislavValentaIng.thismonth) == 2)){
+    document.getElementById("6jmeno").innerHTML = StanislavValentaIng.first + " " + StanislavValentaIng.last;
+    document.getElementById("6vek").innerHTML = StanislavValentaIng.age;
+    document.getElementById("6narozeniny").innerHTML = StanislavValentaIng.date.getDate() + "." + StanislavValentaIng.date.getMonth() + 
+    "." + StanislavValentaIng.date.getFullYear();
+    document.getElementById("6svatek").innerHTML = StanislavValentaIng.sdate.getDate() + "." + StanislavValentaIng.sdate.getMonth();
+    document.getElementById("6narozeniny").style.backgroundColor = "red";
+  }
+  else{
+    document.getElementById("StanislavValentaIng").style.display = "none";
+  }
+}
+function myFunction8() {
+  if (((Meddie.month - Meddie.thismonth) == 1) || ((Meddie.month - Meddie.thismonth) == 2)){
+    document.getElementById("7jmeno").innerHTML = Meddie.first + " " + Meddie.last;
+    document.getElementById("7vek").innerHTML = Meddie.age;
+    document.getElementById("7narozeniny").innerHTML = Meddie.date.getDate() + "." + Meddie.date.getMonth() + 
+    "." + Meddie.date.getFullYear();
+    document.getElementById("7svatek").innerHTML = Meddie.sdate.getDate() + "." + Meddie.sdate.getMonth();
+    document.getElementById("7narozeniny").style.backgroundColor = "red";
+  }
+  else{
+    document.getElementById("Meddie").style.display = "none";
+  }
+}
+function myFunction9() {
+  if (((StanislavValenta.month - StanislavValenta.thismonth) == 1) || ((StanislavValenta.month - StanislavValenta.thismonth) == 2)){
+    document.getElementById("8jmeno").innerHTML = StanislavValenta.first + " " + StanislavValenta.last;
+    document.getElementById("8vek").innerHTML = StanislavValenta.age;
+    document.getElementById("8narozeniny").innerHTML = StanislavValenta.date.getDate() + "." + StanislavValenta.date.getMonth() + 
+    "." + StanislavValenta.date.getFullYear();
+    document.getElementById("8svatek").innerHTML = StanislavValenta.sdate.getDate() + "." + StanislavValenta.sdate.getMonth();
+    document.getElementById("8narozeniny").style.backgroundColor = "red";
+  }
+  else{
+    document.getElementById("StanislavValenta").style.display = "none";
+  }
+}
