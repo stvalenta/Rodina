7099fabf17f83bf451ec7cc17e323b070e270bc2
stvalenta <stvalenta@seznam.cz>
2021-10-13 20:05:09 +0200

    kompletní verze

--- konstruktor.js
+++ konstruktor.js
@@ -13,22 +13,17 @@ function Osoba (first, last, day, month, year, town, sday, smonth, bjmeno){
 	this.thisyear = this.adate.getFullYear();
 	thismonth = this.adate.getMonth();
 	thisday = this.adate.getDate();
-	age = this.thisyear - year;
-	if ((((1 + thismonth) == this.month)) && (thisday == this.day)){
+	age = this.thisyear - year -1;
+	if ((((1 + thismonth) >= this.month)) && (thisday >= this.day)){
 		age += 1;
 	}
 	this.index = this.month - month;
 	this.sindex = this. month - smonth;
-	this.bhtml = this.first + this.last;
-	this.shtml = this.bhtml + "1";
 	this.bjmeno = bjmeno;
 	bvek = bjmeno + bjmeno;
 	bnarozeniny = bvek + bjmeno;
 	bsvatek = bnarozeniny + bjmeno;
 	chtml = bsvatek + this.bjmeno;
-	if ((((1 + thismonth) == this.month)) && (thisday < this.day)){
-	         	document.getElementById(chtml).style.display = "none";
-	         }
 
 	function myFunctionx() {
          if ((((month - thismonth) == 1) || ((month - thismonth) == 2)) || 
@@ -38,14 +33,27 @@ function Osoba (first, last, day, month, year, town, sday, smonth, bjmeno){
            document.getElementById(bnarozeniny).innerHTML = date.getDate() + "." + date.getMonth() + 
            "." + date.getFullYear();
            document.getElementById(bsvatek).innerHTML = sdate.getDate() + "." + sdate.getMonth();
+           
            if (((month - thismonth) == 1) || ((month - thismonth) == 2)){
            	document.getElementById(bnarozeniny).style.backgroundColor = "red";
+           	if (((month - thismonth) == 1) && (thisday > day)){
+           		document.getElementById(bnarozeniny).style.backgroundColor = "grey";
+           	}
            }
            if (((smonth - thismonth) == 1) || ((smonth - thismonth) == 2)){
            	document.getElementById(bsvatek).style.backgroundColor = "red";
+           	if (((smonth - thismonth) == 1) && (thisday > sday)){
+           		document.getElementById(bsvatek).style.backgroundColor = "gray";
+           	}
            }
          }
          else{
+         	document.getElementById(chtml).style.display = "none";
+         }
+         if ((((((((month - thismonth) == 1) && (((smonth - thismonth) != 1) || (smonth - thismonth) != 2))) || 
+         	(((month - thismonth) != 1) || (month - thismonth) != 2)) && ((smonth - thismonth) == 1))  && 
+         	(thisday > day) && 
+         	(thisday > sday))){
          	document.getElementById(chtml).style.display = "none";
          }
     }
