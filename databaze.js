13ae4e0dfbc81fdc43f8622410837fc72056de5a
stvalenta <stvalenta@seznam.cz>
2021-10-13 08:24:14 +0200

    finále

---
+++ databaze.js
@@ -0,0 +1,10 @@
+const PetrSteier = new Osoba("Petr", "Steier", 29, 7, 1976, "Vodňany", 22, 2, "x");
+const PavlaSteierova = new Osoba("Pavla", "Steierová", 17, 7, 1976, "Vodňany", 22, 6, "a");
+const AnetaSteierova = new Osoba("Aneta", "Steierová", 11, 9, 2008, "Vodňany", 17, 5, "b");
+const EvaKorbuthova = new Osoba("Eva", "Korbuthová", 20, 9, 1947, "Vodňany", 24, 12, "c");
+const OlgaValentova = new Osoba("Olga", "Valentová", 7, 3, 1958, "Vodňany", 11, 7, "d");
+const StanislavValentaIng = new Osoba("Stanislav", "Valenta", 29, 12, 1952, "Vodňany", 7, 5, "e");
+const StanislavValenta = new Osoba("Stáník", "Valenta", 3, 5, 1989, "Budyně", 7, 5, "f");
+const EliskaSteierova = new Osoba("Eliška", "Steierová", 12, 9, 2001, "Budyňě", 5, 10, "g");
+const Meddie = new Osoba("Medard", "Hňup", 25, 4, 2019, "Budyně", 8, 6, "h");
+const JanDlouhy = new Osoba("Jan", "Dlouhý", 22, 11, 1981, "Útěšov", 24, 6, "i");
--- konstruktor.js
+++ konstruktor.js
@@ -1,5 +1,4 @@
-function Osoba (first, last, day, month, year, town, sday, smonth, bjmeno, bvek, 
-	bnarozeniny, bsvatek){
+function Osoba (first, last, day, month, year, town, sday, smonth, bjmeno){
 	this.first = first;
 	this.last = last;
 	this.day = day;
@@ -21,14 +20,14 @@ function Osoba (first, last, day, month, year, town, sday, smonth, bjmeno, bvek,
 	this.shtml = this.bhtml + "1";
 	this.bjmeno = bjmeno;
 	sjmeno = this.bjmeno + "1";
-	this.bvek = bvek;
-	svek = this.bvek + "1";
-	this.bnarozeniny = bnarozeniny;
-	snarozeniny = this.bnarozeniny + "1";
-	this.bsvatek = bsvatek;
-	ssvatek = this.bsvatek + "1";
-	chtml = this.bsvatek + this.bjmeno;
-	schtml = this.bsvatek + this.bjmeno + "1";
+	bvek = bjmeno + bjmeno;
+	svek = bvek + "1";
+	bnarozeniny = bvek + bjmeno;
+	snarozeniny = bnarozeniny + "1";
+	bsvatek = bnarozeniny + bjmeno;
+	ssvatek = bsvatek + "1";
+	chtml = bsvatek + this.bjmeno;
+	schtml = bsvatek + this.bjmeno + "1";
 	function myFunctionx() {
          if (((month - thismonth) == 1) || ((month - thismonth) == 2)){
            document.getElementById(bjmeno).innerHTML = first + " " + last;
@@ -59,22 +58,10 @@ function Osoba (first, last, day, month, year, town, sday, smonth, bjmeno, bvek,
     myFunctiony();
 }
 
-const PetrSteier = new Osoba("Petr", "Steier", 29, 7, 1976, "Vodňany", 22, 2, "x", "xx", "xxx", "xxxx");
-const PavlaSteierova = new Osoba("Pavla", "Steierová", 17, 7, 1976, "Vodňany", 22, 6, "a", "aa", "aaa", "aaaa");
-const AnetaSteierova = new Osoba("Aneta", "Steierová", 11, 9, 2008, "Vodňany", 17, 5, "b", "bb", "bbb", "bbbb");
-const EvaKorbuthova = new Osoba("Eva", "Korbuthová", 20, 9, 1947, "Vodňany", 24, 12, "c", "cc", "ccc", "cccc");
-const OlgaValentova = new Osoba("Olga", "Valentová", 7, 3, 1958, "Vodňany", 11, 7, "d", "dd", "ddd", "dddd");
-const StanislavValentaIng = new Osoba("Stanislav", "Valenta", 29, 12, 1952, "Vodňany", 7, 5, "e", "ee", "eee", "eeee");
-const StanislavValenta = new Osoba("Stáník", "Valenta", 3, 5, 1989, "Budyně", 7, 5, "f", "ff", "fff", "ffff");
-const EliskaSteierova = new Osoba("Eliška", "Steierová", 12, 9, 2001, "Budyňě", 5, 10, "g", "gg", "ggg", "gggg");
-const Meddie = new Osoba("Medard", "Hňup", 25, 4, 2019, "Budyně", 8, 6, "h", "hh", "hhh", "hhhh");
-const JanDlouhy = new Osoba("Jan", "Dlouhý", 22, 11, 1981, "Útěšov", 24, 6, "i", "ii", "iii", "iiii");
 
 
-
-
-document.getElementById("demo").innerHTML = PetrSteier.adate.getDate() + "." + PetrSteier.adate.getMonth() + "." 
-+ PetrSteier.adate.getFullYear();
+let casik = new Date();
+document.getElementById("demo").innerHTML = casik.getDate() + "." + casik.getMonth() + "." + casik.getFullYear();
 
 
 
